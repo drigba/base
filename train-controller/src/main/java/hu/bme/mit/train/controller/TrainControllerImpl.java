79511a20aa69aa880cfd8439ab73fa933f874947
drigba <banfizsombor1999@gmail.com>
2022-05-02 15:26:30 +0200

    Thread followspeed impl

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,7 +7,22 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Thread thread;
 
+	public TrainControllerImpl(){
+		thred = new Thread(){
+			public void run(){
+				thread.run();
+				try{
+					followSpeed();
+					thread.sleep(2000);
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		};
+	}
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
