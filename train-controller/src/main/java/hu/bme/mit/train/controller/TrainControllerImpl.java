25dfb86d63932c782515be438c6f66423644dddc
drigba <banfizsombor1999@gmail.com>
2022-05-02 15:37:27 +0200

    missing imports

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,7 +1,8 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
-
+import java.util.Timer;
+import java.util.TimerTask;
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
